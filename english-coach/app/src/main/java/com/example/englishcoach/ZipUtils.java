5855185a18e9f5462a6fe9eda255da5969da7857
JKguanghui <guanghuifoods@outlook.com>
2026-07-15 08:55:03 +0800

    update english-coach/app/src/main/java/com/example/englishcoach/ZipUtils.java

---
+++ english-coach/app/src/main/java/com/example/englishcoach/ZipUtils.java
@@ -0,0 +1,28 @@
+package com.example.englishcoach;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ZipUtils {
+    public static void unzip(File zipFile, File destDir) throws IOException {
+        byte[] buffer = new byte[8192];
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                File outFile = new File(destDir, entry.getName());
+                if (entry.isDirectory()) {
+                    outFile.mkdirs();
+                } else {
+                    outFile.getParentFile().mkdirs();
+                    try (FileOutputStream fos = new FileOutputStream(outFile)) {
+                        int len;
+                        while ((len = zis.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+                }
+                zis.closeEntry();
+            }
+        }
+    }
+}
